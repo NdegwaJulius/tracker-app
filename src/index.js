038e402d3d0f1b5afbfc28ba0103d9295da6d8ee
NdegwaJM <juliusndegwa2@gmail.com>
2021-04-04 08:20:42 +0300

    MongoDb connect, nodemon for automatic restarts,postman use,handling json data,defining user schema,, creating and saving a user & error handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
+require('./models/User');
 const express = require  ('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app =  express ();
-'mongodb+srv://admin:admin@cluster0.r47vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+
+
+const mongoUri = 'mongodb+srv://admin:plun@cluster0.paes2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+    
+   useNewUrlParser: true,
+   useCreateIndex: true 
+}); 
+
+
+mongoose.connection.on('connected', () => {
+    console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', err => {
+    console.error('error connceting to mongo', err);
+});
 app.get('/', (req, res) => {
     res.send('Hi there !');
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000')
+    console.log('Listening on port 3000');
 });
